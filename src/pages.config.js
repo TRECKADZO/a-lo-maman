f8a0ca1d12fd233e830b6588925853e88f601ad5
Klenakan Eric Traore <klenakan.eric@gmail.com>
2025-12-10 12:59:44 +0000

    Standards interopérabilité A'lo Maman

--- src/pages.config.js
+++ src/pages.config.js
@@ -43,7 +43,6 @@ import FamilleConnectee from './pages/FamilleConnectee';
 import MesDocuments from './pages/MesDocuments';
 import VideoConsultation from './pages/VideoConsultation';
 import Ressources from './pages/Ressources';
-import Interoperabilite from './pages/Interoperabilite';
 import __Layout from './Layout.jsx';
 
 
@@ -93,7 +92,6 @@ export const PAGES = {
     "MesDocuments": MesDocuments,
     "VideoConsultation": VideoConsultation,
     "Ressources": Ressources,
-    "Interoperabilite": Interoperabilite,
 }
 
 export const pagesConfig = {

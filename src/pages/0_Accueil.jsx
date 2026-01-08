7206eb3ac4f841baae16f3373b227bb7468b884c
Klenakan Eric Traore <klenakan.eric@gmail.com>
2026-01-08 16:14:54 +0000

    File changes

--- src/pages/0_Accueil.jsx
+++ src/pages/0_Accueil.jsx
@@ -398,6 +398,15 @@ export default function Accueil() {
         </div>
       </div>
 
+      {/* CTA Déclaration Naissance - Pour utilisateurs connectés */}
+      {user && profiles?.maman && (
+        <div className="py-16 bg-gradient-to-br from-pink-50 to-purple-50">
+          <div className="max-w-4xl mx-auto px-4">
+            <NaissanceCTA variant="full" />
+          </div>
+        </div>
+      )}
+
       {/* Fonctionnalités */}
       <div className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
